5163209fa5df459631ed9efe91fd21310544e3e2
RafyAmgadBenjamin <46320202+RafyAmgadBenjamin@users.noreply.github.com>
2020-08-06 23:03:43 +0200

    Delete environmnet_variable.sh

--- udacity-c3-restapi/environmnet_variable.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export POSTGRESS_USERNAME=udagramrutnerdev
-export POSTGRESS_PASSWORD=udagramrutnerdev
-export POSTGRESS_DB=udagramruttnerdev
-export POSTGRESS_HOST=udagramrutnerdev.cgdgvnv71x07.us-west-2.rds.amazonaws.com
-export AWS_REGION=us-west-2
-export AWS_PROFILE=default
-export AWS_BUCKET=udagram-ruttner-rafy-dev
-export JWT_SECRET=helloworld
